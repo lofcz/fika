298eff85d11659322111d35fdcc1648abbb97acd
lofcz <lofcz@users.noreply.github.com>
2026-08-10 05:49:10 +0000

    deploy to github pages fbe85057d2e656741dffa627e1d3851e30b0809f

---
+++ assets/swimlanesDiagram-ULZ7WXOC-DEu-HVdu.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-Gr2DtdLx.js";import"./chunk-I66GZJ75-BmrZX2mZ.js";import"./chunk-NSK5VX7P-Qmxo7Kxw.js";import"./chunk-4I5QYGJK-lEfgK8Ii.js";import"./chunk-WRU74C26-C-LO2fao.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-UBXNYLIW-DvC7nGoF.js";import"./chunk-2GRJ4B5K-B6ygFqlV.js";import"./chunk-XXDRQBXY-Mbzz_cet.js";import"./chunk-KBJHAD2P-BgLEyTlc.js";import"./chunk-W5SLKNZC-BfgCegI0.js";import"./chunk-QR6OTTB3-C3pvndMv.js";import"./chunk-7Z6QIM7H-BaATFpaR.js";import"./chunk-J7OUQ5F2-C1IEKp09.js";import"./chunk-ZIRB5QZD-H-kepumB.js";import{r as t,t as n}from"./chunk-JQJVKLGR-DrfMzVyU.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
